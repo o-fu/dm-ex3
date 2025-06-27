c0c0ca644dde29e1d5477ae2f8bd71a8ccdd49e3
o-fu <40477825+o-fu@users.noreply.github.com>
2025-06-27 11:09:27 +0900

    最初のコミットinitial commit:

---
+++ dice_Game.c
@@ -0,0 +1,29 @@
+ｋｋｋｋｋｋｋｋｋｋｋｋｋｋ
+
+/*
+	Dice game 
+	2025-6
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	int die_1, die_2;
+
+	srand((unsigned int)time(NULL));
+
+
+	printf("Rolling dice...\n");
+	
+	die_1 = rand() % 6 + 1;
+	printf("Die 1: %d\n", die_1);
+	
+	die_2 = rand() % 6 + 1;
+	printf("Die 2: %d\n", die_2);
+
+	printf("Total Value: %d\n", die_1 + die_2);
+
+  return 0;
+}
